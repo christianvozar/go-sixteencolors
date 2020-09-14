3466986c6901886bd04b952776614f385665ba0d
Christian R. Vozar <christian@rogueethic.com>
2020-09-13 21:05:00 -0600

    src: initial scaffolding

---
+++ sixteencolors/sixteencolors.go
@@ -0,0 +1,18 @@
+package sixteencolors
+
+const (
+	APIVersion     = "v1"
+	DefaultBaseURL = "https://api.16colo.rs/"
+)
+
+/*
+/v1/pack
+/v1/pack/:name
+/v1/year
+/v1/year/:year
+/v1/group
+/v1/group/:name
+/v1/artist
+/v1/artist/:name
+/v1/paste
+*/
